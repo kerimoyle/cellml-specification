f9142f8b0c32dd8b86ae51d51ea0b2e2a376af98
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-04-02 16:15:34 +1300

    Delete empty line in conf.py.

--- src/conf.py
+++ src/conf.py
@@ -191,7 +191,6 @@ release = '1.0.0-rc1'
 # directories to ignore when looking for source files.
 exclude_patterns = define_excluded_patterns()
 
-
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
 #default_role = None
